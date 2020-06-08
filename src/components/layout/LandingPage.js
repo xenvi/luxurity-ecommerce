101498f555d3091cbb24737c0cf16702df7747c7
tiffany t <iluvpups20@gmail.com>
2020-06-07 22:11:50 -0500

    Added video landing page background

--- src/components/layout/LandingPage.js
+++ src/components/layout/LandingPage.js
@@ -4,16 +4,12 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 // image uploads
-import dress_landing from "../../images/dress_landing.jpg";
+//import dress_landing from "../../images/dress_landing.jpg";
+import dress_landing from "../../dress.mp4";
 
 const Landing = styled.section`
   width: 100%;
   height: 95vh;
-  background-image: url("${dress_landing}");
-  background-size: cover;
-  background-position: 25% center;
-  background-attachment: fixed;
-  background-repeat: no-repeat;
   padding: 0 5em;
   align-content: center;
 `;
@@ -22,31 +18,57 @@ const LandingText = styled.p`
   text-align: center;
   line-height: 100%;
   color: #fff;
-  text-shadow: 0 0 0.1em #000;
+  text-shadow: 0 0.05em 0.1em rgba(0,0,0,0.5);
+  z-index: 2;
 `;
 const LandingButton = styled(Link)`
   padding: 1em 2em;
-  background: #af9e73;
+  background: none;
   color: #fff;
   font-weight: bold;
   font-size: 1.1em;
   cursor: pointer;
-  box-shadow: 0 0 0.7em #af9e73;
+  border: 2px solid #af9e73;
   transition: all 0.2s ease-in-out;
-  margin-top: 1.5em;
-
+  z-index: 2;
+  letter-spacing: 3px;
+  margin-top: 1em;
   &:hover {
     text-decoration: none;
-    background: #333;
-    color: #af9e73 !important;
+    background: rgba(175, 158, 115, 0.3);
+    color: #fff;
+    box-shadow: 0 0 0.5em #af9e73;
     transition: all 0.2s ease-in-out;
   }
+`;
+
+const VideoWrap = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
+const Overlay = styled.div`
+  z-index: 1;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  position: absolute;
+  background: #000;
+  opacity: 0.5;
 `;
 
 export class LandingPage extends Component {
   render() {
     return (
       <Landing className="flex flex-wrap landing_container">
+        <VideoWrap>
+          <video src={dress_landing} autoplay="" loop="true" muted />
+        </VideoWrap>
+        <Overlay />
         <LandingText className="fancytext landingtext">
           Intricately woven dresses made perfect for you.
         </LandingText>
