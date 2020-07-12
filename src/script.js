13a6b34ac8ff22f770d1b693f46c8a452ed29392
tiffany t <iluvpups20@gmail.com>
2020-07-12 18:46:01 -0500

    Fix netlify redirects not working with React Router

--- src/script.js
+++ src/script.js
@@ -1,8 +1,9 @@
 window.onscroll = function () {
   var nav = document.querySelector("nav");
-  if (window.pageYOffset > 75) {
-    nav.classList.add("stickynav");
-  } else {
-    nav.classList.remove("stickynav");
-  }
+  if (window.pageYOffset > 118) {
+      nav.classList.add("stickynav");
+    } else {
+      nav.classList.remove("stickynav");
+    }
+  
 };
